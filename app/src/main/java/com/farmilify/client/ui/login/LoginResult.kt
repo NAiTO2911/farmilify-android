e78b63286e25872ab9a02d876b8e4a6d9bb2fc1a
muhfelmi <felmiadi@gmail.com>
2021-04-30 13:16:22 +0700

    Updated code

---
+++ app/src/main/java/com/farmilify/client/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.farmilify.client.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
