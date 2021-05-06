29ef00d42e5257c64c10ab55b7ef50a2127ed67f
muhfelmi <felmiadi@gmail.com>
2021-05-06 12:13:55 +0700

    Updated code

--- app/src/main/java/com/farmilify/client/Landing.kt
+++ app/src/main/java/com/farmilify/client/Landing.kt
@@ -4,21 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
-        val button = findViewById<Button>(R.id.login)
-        button.setOnClickListener {
+        val buttonLog = findViewById<Button>(R.id.keLogin)
+        buttonLog.setOnClickListener {
             val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-        }
+            startActivity(intent) }
+
+        val buttonDaftar = findViewById<Button>(R.id.keDaftar)
+        buttonDaftar.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent) }
     }
+
 }
--- app/src/main/java/com/farmilify/client/Register.kt
+++ app/src/main/java/com/farmilify/client/Register.kt
@@ -1,22 +1,16 @@
 package com.farmilify.client
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
-class Login : AppCompatActivity() {
+class Register : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val button = findViewById<Button>(R.id.loginbutton)
-        button.setOnClickListener {
-            val intent = Intent(this, Dashboard::class.java)
-            startActivity(intent) }
-        }
     }
+}
